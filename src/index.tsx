aabc4b817c7442d7ca7dd38977fd55f48f6a7cbf
Christian Schleidt <cschleidt@hotmail.com>
2023-02-25 15:45:32 +0100

    Fix env vars

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN || 'dev-wf53ttsqsuq5jb6r.us.auth0.com'}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || '4zNgQagFYC4ft1uAYOqGbET1TgG31r5L'}
-    redirectUri='https://inviewer.azurewebsites.net/profile'
+    redirectUri={process.env.REACT_APP_INVIEW_REDIRCT_URL}
   >
     <App />
   </Auth0Provider>,
